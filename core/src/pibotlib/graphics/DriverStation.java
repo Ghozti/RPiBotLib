80c51ddb577ddc51b2a7db65d6fa8695316b3739
Ghozti <62896836+ghozti@users.noreply.github.com>
2022-06-11 14:57:15 -0400

    - jar tetsing.,

--- core/src/pibotlib/graphics/DriverStation.java
+++ core/src/pibotlib/graphics/DriverStation.java
@@ -34,7 +34,7 @@ public class DriverStation implements Screen {
         img = new Texture(Gdx.files.internal("PiBotLib Driver Station.png"));
         enableButton = new DriverStationButton("Enable (1).png",80,50);
         disableButton = new DriverStationButton("disable.png",280,50);
-        mouseHitbox = new com.badlogic.gdx.math.Rectangle(Gdx.input.getX(),-Gdx.input.getY(),15,15);
+        mouseHitbox = new com.badlogic.gdx.math.Rectangle(Gdx.input.getX()*100,-Gdx.input.getY()*100,15,15);
         font = new Font(100);
         enableSound = Gdx.audio.newMusic(Gdx.files.internal("autonstart.mp3"));
         disableSound = Gdx.audio.newMusic(Gdx.files.internal("buzzer.mp3"));
