c669ef73f25e3f935a3af65ca3d6597e1bdeb7ab
Ghozti <62896836+ghozti@users.noreply.github.com>
2022-06-11 15:23:36 -0400

    - jar tetsing...

--- core/src/pibotlib/utils/drives/DifferentialDrive.java
+++ core/src/pibotlib/utils/drives/DifferentialDrive.java
@@ -19,19 +19,24 @@ public class DifferentialDrive extends ChassisDrive{
             getLeftController().motor2Backward(-x);
             getRightController().motor1Backward(-x);
             getRightController().motor2Backward(-x);
-        }else if(y < 0) {//left turn
+        } else {//halt
+            getLeftController().motor1Kill();
+            getRightController().motor2Kill();
+        }
+
+        if(y < 0) {//left turn
             System.out.println("left");
             getLeftController().motor1Backward(-y);
             getLeftController().motor2Backward(-y);
             getRightController().motor1Forward(-y);
             getRightController().motor2Forward(-y);
-        }else if(y > 0){//right turn
+        }else if(y > 0) {//right turn
             System.out.println("right");
             getRightController().motor1Backward(y);
             getRightController().motor2Backward(y);
             getLeftController().motor1Forward(y);
             getLeftController().motor2Forward(y);
-        }else {//halt
+        } else {//halt
             getLeftController().motor1Kill();
             getRightController().motor2Kill();
         }
