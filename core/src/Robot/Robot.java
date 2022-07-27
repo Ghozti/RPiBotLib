e8e52d9613bccdff7dbbc67a92a3f167463626b3
Ghozti <62896836+ghozti@users.noreply.github.com>
2022-07-27 16:36:22 -0400

    - updated jar..

--- core/src/Robot/Robot.java
+++ core/src/Robot/Robot.java
@@ -25,55 +25,56 @@ public class Robot extends TimedRobotBase {
 
     public Robot(){
         //constructor called once which can create its own controller
-        try {
-            controller = new LocalXboxController();
-            controllerFound = true;
-        }catch (Exception e){
-            System.out.println("No controller detected");
-            controllerFound = false;
-        }
+        //try {
+        //    controller = new LocalXboxController();
+        //    controllerFound = true;
+        //}catch (Exception e){
+        //    System.out.println("No controller detected");
+        //    controllerFound = false;
+        //}
     }
 
     public Robot(LocalXboxController controller){
         //constructor which takes a controller as an argument
-        try {
-            controllerFound = true;
-            this.controller = controller;
-        }catch (Exception e){
-            System.out.println("No controller detected");
-            controllerFound = false;
-        }
+        //try {
+        //    controllerFound = true;
+        //    this.controller = controller;
+        //}catch (Exception e){
+        //    System.out.println("No controller detected");
+        //    controllerFound = false;
+        //}
     }
 
     @Override
     public void robotInit() {
-        //try {
-        //    context = Pi4J.newAutoContext();//always call first
-//
-        //    leftController = new DualHBridgeController(context, 14, 15, 23, 24);
-        //    rightController = new DualHBridgeController(context, 9, 25, 11, 8);
-//
-        //    leftController.configMotor1PWM(PwmConfig.buildPwmConfig(leftController.getContext(), 18, PwmType.HARDWARE));
-        //    leftController.configMotor2PWM(PwmConfig.buildPwmConfig(leftController.getContext(), 12, PwmType.HARDWARE));
-        //    rightController.configMotor1PWM(PwmConfig.buildPwmConfig(rightController.getContext(), 13, PwmType.HARDWARE));
-        //    rightController.configMotor2PWM(PwmConfig.buildPwmConfig(rightController.getContext(), 19, PwmType.HARDWARE));
-//
-        //    leftController.setMotor1DigitalForward(DigitalOutputConfig.buildDigitalOutputConfig(leftController.getContext(), 23, "pin23", "left motorL"));
-        //    leftController.setMotor1DigitalBackward(DigitalOutputConfig.buildDigitalOutputConfig(leftController.getContext(), 24, "pin24", "left motorL"));
-        //    leftController.setMotor2DigitalForward(DigitalOutputConfig.buildDigitalOutputConfig(leftController.getContext(), 25, "pin25", "left motorR"));
-        //    leftController.setMotor2DigitalBackward(DigitalOutputConfig.buildDigitalOutputConfig(leftController.getContext(), 8, "pin8", "left motorR"));
-//
-        //    rightController.setMotor1DigitalForward(DigitalOutputConfig.buildDigitalOutputConfig(rightController.getContext(), 17, "pin17", "left motorL"));
-        //    rightController.setMotor1DigitalBackward(DigitalOutputConfig.buildDigitalOutputConfig(rightController.getContext(), 27, "pin27", "left motorL"));
-        //    rightController.setMotor2DigitalForward(DigitalOutputConfig.buildDigitalOutputConfig(rightController.getContext(), 22, "pin22", "left motorR"));
-        //    rightController.setMotor2DigitalBackward(DigitalOutputConfig.buildDigitalOutputConfig(rightController.getContext(), 10, "pin10", "left motorR"));
-//
-        //    differentialDrive = new DifferentialDrive(leftController, rightController);
-        //    stateLight = new RobotStateLight(context, 16);
-        //    autoBase = new TimedAutoBase();
-        //}catch (Exception e){
-        //    System.out.println("Robot init fail, reboot raspberry pi and try again");
-        //}
+        try {
+            System.out.println("robot init");
+            context = Pi4J.newAutoContext();//always call first
+
+            leftController = new DualHBridgeController(context, 14, 15, 23, 24);
+            rightController = new DualHBridgeController(context, 9, 25, 11, 8);
+
+            leftController.configMotor1PWM(PwmConfig.buildPwmConfig(leftController.getContext(), 18, PwmType.HARDWARE));
+            leftController.configMotor2PWM(PwmConfig.buildPwmConfig(leftController.getContext(), 12, PwmType.HARDWARE));
+            rightController.configMotor1PWM(PwmConfig.buildPwmConfig(rightController.getContext(), 13, PwmType.HARDWARE));
+            rightController.configMotor2PWM(PwmConfig.buildPwmConfig(rightController.getContext(), 19, PwmType.HARDWARE));
+
+            leftController.setMotor1DigitalForward(DigitalOutputConfig.buildDigitalOutputConfig(leftController.getContext(), 23, "pin23", "left motorL"));
+            leftController.setMotor1DigitalBackward(DigitalOutputConfig.buildDigitalOutputConfig(leftController.getContext(), 24, "pin24", "left motorL"));
+            leftController.setMotor2DigitalForward(DigitalOutputConfig.buildDigitalOutputConfig(leftController.getContext(), 25, "pin25", "left motorR"));
+            leftController.setMotor2DigitalBackward(DigitalOutputConfig.buildDigitalOutputConfig(leftController.getContext(), 8, "pin8", "left motorR"));
+
+            rightController.setMotor1DigitalForward(DigitalOutputConfig.buildDigitalOutputConfig(rightController.getContext(), 17, "pin17", "left motorL"));
+            rightController.setMotor1DigitalBackward(DigitalOutputConfig.buildDigitalOutputConfig(rightController.getContext(), 27, "pin27", "left motorL"));
+            rightController.setMotor2DigitalForward(DigitalOutputConfig.buildDigitalOutputConfig(rightController.getContext(), 22, "pin22", "left motorR"));
+            rightController.setMotor2DigitalBackward(DigitalOutputConfig.buildDigitalOutputConfig(rightController.getContext(), 10, "pin10", "left motorR"));
+
+            differentialDrive = new DifferentialDrive(leftController, rightController);
+            stateLight = new RobotStateLight(context, 16);
+            autoBase = new TimedAutoBase();
+        }catch (Exception e){
+            System.out.println("Robot init fail, reboot raspberry pi and try again");
+        }
     }
 
     @Override
@@ -90,22 +91,22 @@ public class Robot extends TimedRobotBase {
 
     @Override
     public void autonomousPeriodic() {
-        //stateLight.blinkRSL();
-        //autoBase.addCommand(new TimedCommand(2000L,differentialDrive,50,0));
-        //autoBase.addCommand(new TimedCommand(2000L,differentialDrive,0,0));
-        //autoBase.runAuto();
+        stateLight.blinkRSL();
+        autoBase.addCommand(new TimedCommand(2000L,differentialDrive,50,0));
+        autoBase.addCommand(new TimedCommand(2000L,differentialDrive,0,0));
+        autoBase.runAuto();
     }
 
     @Override
     public void teleopPeriodic() {
         //differentialDrive.arcadeDrive(-controller.getLeftYAxis() * 100, controller.getRightYAxis() * 100);
-        //stateLight.blinkRSL();
+        stateLight.blinkRSL();
     }
 
     @Override
     public void robotShutDown() {
-        //stateLight.shutDown();
-        //differentialDrive.arcadeDrive(0, 0);
-        //context.shutdown();
+        stateLight.shutDown();
+        differentialDrive.arcadeDrive(0, 0);
+        context.shutdown();
     }
 }
--- desktop/src/pibotlib/desktop/DesktopLauncher.java
+++ desktop/src/pibotlib/desktop/DesktopLauncher.java
@@ -1,18 +1,22 @@
 package pibotlib.desktop;
 
+import Robot.Robot;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import pibotlib.DriverStationLauncher;
+import pibotlib.lib.gamecontrollers.LocalXboxController;
 import pibotlib.lib.time.ElapseTimer;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setTitle("PiBot lib driver station");
-		config.setWindowSizeLimits(1080,720,1080,720);
-		config.setWindowedMode(1080,720);
-		config.setForegroundFPS(30);
-		config.setIdleFPS(10);
-		new Lwjgl3Application(new DriverStationLauncher(), config);
+		//Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		//config.setTitle("PiBot lib driver station");
+		//config.setWindowSizeLimits(1080,720,1080,720);
+		//config.setWindowedMode(1080,720);
+		//config.setForegroundFPS(30);
+		//config.setIdleFPS(10);
+		//new Lwjgl3Application(new DriverStationLauncher(), config);
+		Thread thread = new Thread(new Robot());
+		thread.start();
 	}
 }
