0ec6428b1a359c5a4eca56bebb5929c1b9602d2c
Ghozti <62896836+ghozti@users.noreply.github.com>
2022-07-29 16:37:18 -0400

    - jar test

--- core/src/Robot/Robot.java
+++ core/src/Robot/Robot.java
@@ -52,28 +52,28 @@ public class Robot extends TimedRobotBase {
         try {
             context = Pi4J.newAutoContext();//always call first
 
-            leftController = new DualHBridgeController(context, 14, 15, 23, 24);
-            rightController = new DualHBridgeController(context, 9, 25, 11, 8);
+            //leftController = new DualHBridgeController(context, 14, 15, 23, 24);
+            //rightController = new DualHBridgeController(context, 9, 25, 11, 8);
 
-            leftController.configMotor1PWM(PwmConfig.buildPwmConfig(leftController.getContext(), 18, PwmType.HARDWARE));
-            leftController.configMotor2PWM(PwmConfig.buildPwmConfig(leftController.getContext(), 12, PwmType.HARDWARE));
-            rightController.configMotor1PWM(PwmConfig.buildPwmConfig(rightController.getContext(), 13, PwmType.HARDWARE));
-            rightController.configMotor2PWM(PwmConfig.buildPwmConfig(rightController.getContext(), 19, PwmType.HARDWARE));
+            //leftController.configMotor1PWM(PwmConfig.buildPwmConfig(leftController.getContext(), 18, PwmType.HARDWARE));
+            //leftController.configMotor2PWM(PwmConfig.buildPwmConfig(leftController.getContext(), 12, PwmType.HARDWARE));
+            //rightController.configMotor1PWM(PwmConfig.buildPwmConfig(rightController.getContext(), 13, PwmType.HARDWARE));
+            //rightController.configMotor2PWM(PwmConfig.buildPwmConfig(rightController.getContext(), 19, PwmType.HARDWARE));
 
-            leftController.setMotor1DigitalForward(DigitalOutputConfig.buildDigitalOutputConfig(leftController.getContext(), 23, "pin23", "left motorL"));
-            leftController.setMotor1DigitalBackward(DigitalOutputConfig.buildDigitalOutputConfig(leftController.getContext(), 24, "pin24", "left motorL"));
-            leftController.setMotor2DigitalForward(DigitalOutputConfig.buildDigitalOutputConfig(leftController.getContext(), 25, "pin25", "left motorR"));
-            leftController.setMotor2DigitalBackward(DigitalOutputConfig.buildDigitalOutputConfig(leftController.getContext(), 8, "pin8", "left motorR"));
+            //leftController.setMotor1DigitalForward(DigitalOutputConfig.buildDigitalOutputConfig(leftController.getContext(), 23, "pin23", "left motorL"));
+            //leftController.setMotor1DigitalBackward(DigitalOutputConfig.buildDigitalOutputConfig(leftController.getContext(), 24, "pin24", "left motorL"));
+            //leftController.setMotor2DigitalForward(DigitalOutputConfig.buildDigitalOutputConfig(leftController.getContext(), 25, "pin25", "left motorR"));
+            //leftController.setMotor2DigitalBackward(DigitalOutputConfig.buildDigitalOutputConfig(leftController.getContext(), 8, "pin8", "left motorR"));
 
-            rightController.setMotor1DigitalForward(DigitalOutputConfig.buildDigitalOutputConfig(rightController.getContext(), 17, "pin17", "left motorL"));
-            rightController.setMotor1DigitalBackward(DigitalOutputConfig.buildDigitalOutputConfig(rightController.getContext(), 27, "pin27", "left motorL"));
-            rightController.setMotor2DigitalForward(DigitalOutputConfig.buildDigitalOutputConfig(rightController.getContext(), 22, "pin22", "left motorR"));
-            rightController.setMotor2DigitalBackward(DigitalOutputConfig.buildDigitalOutputConfig(rightController.getContext(), 10, "pin10", "left motorR"));
+            //rightController.setMotor1DigitalForward(DigitalOutputConfig.buildDigitalOutputConfig(rightController.getContext(), 17, "pin17", "left motorL"));
+            //rightController.setMotor1DigitalBackward(DigitalOutputConfig.buildDigitalOutputConfig(rightController.getContext(), 27, "pin27", "left motorL"));
+            //rightController.setMotor2DigitalForward(DigitalOutputConfig.buildDigitalOutputConfig(rightController.getContext(), 22, "pin22", "left motorR"));
+            //rightController.setMotor2DigitalBackward(DigitalOutputConfig.buildDigitalOutputConfig(rightController.getContext(), 10, "pin10", "left motorR"));
 
-            differentialDrive = new DifferentialDrive(leftController, rightController);
-            stateLight = new RobotStateLight(context, 16);
+            //differentialDrive = new DifferentialDrive(leftController, rightController);
+            //stateLight = new RobotStateLight(context, 16);
+            //autoBase = new TimedAutoBase();
             sonicSensor = new UltraSonicSensor(context,5,6);
-            autoBase = new TimedAutoBase();
 
             sonicSensor.runSensor();
         }catch (Exception e){
@@ -83,35 +83,35 @@ public class Robot extends TimedRobotBase {
 
     @Override
     public void robotPeriodic() {
-        if (!controllerFound){
-            try {
-                controller = new LocalXboxController();
-                controllerFound = true;
-            }catch (Exception e){
-                System.out.println("No controller found");
-                e.printStackTrace();
-            }
-        }
+        //if (!controllerFound){
+        //    try {
+        //        controller = new LocalXboxController();
+        //        controllerFound = true;
+        //    }catch (Exception e){
+        //        System.out.println("No controller found");
+        //        e.printStackTrace();
+        //    }
+        //}
     }
 
     @Override
     public void autonomousPeriodic() {
-        stateLight.blinkRSL();
-        autoBase.addCommand(new TimedCommand(2000L,differentialDrive,50,0));
-        autoBase.addCommand(new TimedCommand(2000L,differentialDrive,0,0));
-        autoBase.runAuto();
+        //stateLight.blinkRSL();
+        //autoBase.addCommand(new TimedCommand(2000L,differentialDrive,50,0));
+        //autoBase.addCommand(new TimedCommand(2000L,differentialDrive,0,0));
+        //autoBase.runAuto();
     }
 
     @Override
     public void teleopPeriodic() {
-        differentialDrive.arcadeDrive(-controller.getLeftXAxis() * 100, controller.getRightYAxis() * 100);
-        stateLight.blinkRSL();
+        //differentialDrive.arcadeDrive(-controller.getLeftXAxis() * 100, controller.getRightYAxis() * 100);
+        //stateLight.blinkRSL();
     }
 
     @Override
     public void robotShutDown() {
-        stateLight.shutDown();
-        differentialDrive.arcadeDrive(0, 0);
+        //stateLight.shutDown();
+        //differentialDrive.arcadeDrive(0, 0);
         context.shutdown();
     }
 }
