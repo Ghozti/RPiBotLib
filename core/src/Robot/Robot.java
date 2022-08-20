ad53179c9214e733e9b90d996a7f27e25855de91
Ghozti <62896836+ghozti@users.noreply.github.com>
2022-08-19 20:59:57 -0400

    - finished RPiBotLib v1.1.0

--- core/src/Robot/Robot.java
+++ core/src/Robot/Robot.java
@@ -1,38 +1,11 @@
 package Robot;
 
-import com.badlogic.gdx.controllers.Controller;
-import com.badlogic.gdx.controllers.Controllers;
-import com.diozero.api.ServoTrim;
-import com.pi4j.Pi4J;
 import com.pi4j.context.Context;
-import com.pi4j.io.i2c.I2CConfig;
-import com.pi4j.io.i2c.I2CProvider;
-import com.pi4j.io.pwm.PwmType;
-import pibotlib.graphics.DriverStation;
-import pibotlib.lib.addons.DigitalOutputConfig;
-import pibotlib.lib.addons.PwmConfig;
-import pibotlib.lib.addons.RobotStateLight;
 import pibotlib.lib.addons.TimedRobotBase;
-import pibotlib.lib.addons.hats.ServoDriver;
-import pibotlib.lib.addons.sensors.UltraSonicSensor;
-import pibotlib.lib.autonomous.TimedAutoBase;
-import pibotlib.lib.drives.DifferentialDrive;
-import pibotlib.lib.gamecontrollers.LocalXboxController;
-import pibotlib.lib.motorcontrollers.DualHBridgeController;
 
 public class Robot extends TimedRobotBase {
 
     Context context;
-   // LocalXboxController controller;
-    LocalXboxController controller;
-    DualHBridgeController leftController, rightController;
-    DifferentialDrive differentialDrive;
-    RobotStateLight stateLight;
-    UltraSonicSensor sonicSensor;
-    boolean controllerFound;
-
-    TimedAutoBase autoBase;
-    ServoDriver driver;
 
     public Robot(){
 
@@ -41,53 +14,13 @@ public class Robot extends TimedRobotBase {
     //called once per program init
     @Override
     public void robotInit() {
-        try {
-            context = Pi4J.newAutoContext();//always call first
-            controller = new LocalXboxController();
-            driver = new ServoDriver();
-            driver.addAllServos(ServoTrim.MG996R);
 
-            leftController = new DualHBridgeController(context, 14, 15, 23, 24);
-            rightController = new DualHBridgeController(context, 9, 25, 11, 8);
-
-            leftController.configMotor1PWM(PwmConfig.buildPwmConfig(leftController.getContext(), 18, PwmType.HARDWARE));
-            leftController.configMotor2PWM(PwmConfig.buildPwmConfig(leftController.getContext(), 12, PwmType.HARDWARE));
-            rightController.configMotor1PWM(PwmConfig.buildPwmConfig(rightController.getContext(), 13, PwmType.HARDWARE));
-            rightController.configMotor2PWM(PwmConfig.buildPwmConfig(rightController.getContext(), 19, PwmType.HARDWARE));
-
-            leftController.setMotor1DigitalForward(DigitalOutputConfig.buildDigitalOutputConfig(leftController.getContext(), 23, "pin23", "left motorL"));
-            leftController.setMotor1DigitalBackward(DigitalOutputConfig.buildDigitalOutputConfig(leftController.getContext(), 24, "pin24", "left motorL"));
-            leftController.setMotor2DigitalForward(DigitalOutputConfig.buildDigitalOutputConfig(leftController.getContext(), 25, "pin25", "left motorR"));
-            leftController.setMotor2DigitalBackward(DigitalOutputConfig.buildDigitalOutputConfig(leftController.getContext(), 8, "pin8", "left motorR"));
-
-            rightController.setMotor1DigitalForward(DigitalOutputConfig.buildDigitalOutputConfig(rightController.getContext(), 17, "pin17", "left motorL"));
-            rightController.setMotor1DigitalBackward(DigitalOutputConfig.buildDigitalOutputConfig(rightController.getContext(), 27, "pin27", "left motorL"));
-            rightController.setMotor2DigitalForward(DigitalOutputConfig.buildDigitalOutputConfig(rightController.getContext(), 22, "pin22", "left motorR"));
-            rightController.setMotor2DigitalBackward(DigitalOutputConfig.buildDigitalOutputConfig(rightController.getContext(), 10, "pin10", "left motorR"));
-
-            differentialDrive = new DifferentialDrive(leftController, rightController);
-            stateLight = new RobotStateLight(context, 16);
-        }catch (Exception e){
-            System.out.println("Robot init fail, reboot raspberry pi and try again");
-            e.printStackTrace();
-        }
     }
 
     //called periodically at all times
     @Override
     public void robotPeriodic() {
-        if (controller.getDPadUp()){
-            driver.setServoAngle(0,90);
-        }
-        if (controller.getDPadDown()){
-            driver.setServoAngle(0,0);
-        }
-        if (controller.getDPadLeft()){
-            driver.setServoAngle(0,180);
-        }
-        if (controller.getDPadRight()){
-            driver.setServoAngle(0,70);
-        }
+
     }
 
     //called periodically during autonomous
--- core/src/pibotlib/lib/addons/hats/ServoDriver.java
+++ core/src/pibotlib/lib/addons/hats/ServoDriver.java
@@ -1,27 +1,15 @@
 package pibotlib.lib.addons.hats;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.CancellationException;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 import com.diozero.api.I2CConstants;
-import com.diozero.util.SleepUtil;
-import org.tinylog.Logger;
-import com.diozero.animation.Animation;
-import com.diozero.animation.AnimationInstance;
-import com.diozero.animation.easing.Sine;
 import com.diozero.api.ServoDevice;
 import com.diozero.api.ServoTrim;
 import com.diozero.devices.PCA9685;
 
 public class ServoDriver {
 
-    private static final long LARGE_DELAY = 500;
-    private static final long SHORT_DELAY = 10;
-
-    PCA9685 pca9685;
+    private int maxServos = 16;
+    private PCA9685 pca9685;
     private ArrayList<ServoDevice> servos = new ArrayList<>();
 
     public ServoDriver(int constant, int address, int pwmFrequency){
@@ -43,7 +31,7 @@ public class ServoDriver {
     }
 
     public void addServo(int gpio, ServoTrim trim){
-        if (servos.size() >= 12){
+        if (servos.size() >= maxServos){
             System.out.println("servo limit is 12");
             return;
         }else {
@@ -62,7 +50,7 @@ public class ServoDriver {
             return;
         }else {
             try {
-                for (int i = 0; i < 16; i++){
+                for (int i = 0; i < maxServos; i++){
                     servos.add(ServoDevice.newBuilder(i).setDeviceFactory(pca9685).setTrim(trim).build());
                 }
             }catch (Exception e){
